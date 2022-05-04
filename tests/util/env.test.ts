029df4b141f9c58fe05944c39d00baf1d1ed2af1
Ainsley Clark <info@ainsleyclark.com>
2022-05-04 14:05:31 +0100

    Removed logs

--- tests/util/env.test.ts
+++ tests/util/env.test.ts
@@ -2,10 +2,14 @@
 
 describe('Environment', () => {
     it('Should pass validation', () => {
-
     });
     it("Should throw an error when validation is bad", () => {
-
+        try {
+            const env = require("./../../src/util/env");
+            console.log(env)
+        } catch (e) {
+            console.log(e);
+        }
     });
 
 });
